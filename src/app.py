96e9a1dbd768c1f25d8059741e400f32bf440152
jseiglie <javierseiglie@gmail.com>
2022-02-10 15:25:54 +0000

    exercise

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import json
+app = Flask(__name__)
+
+todos = [{"label": "My first task", "done": False }, {"label": "My second task", "done": False }]
+@app.route('/todos', methods=['GET'])
+def get_Todo():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos)
+    
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    del todos[position]
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
